8f123d64dd20b96d135381031c0277c3fcdbeecc
Moiseyenko Varvara Viktorovna <VMoiseenko@beeline.uz>
2020-09-24 17:54:46 +0500

    feat: main layout

---
+++ src/layouts/LayoutMain.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="grid-container">
+    <div class="phrases">
+      <slot name="phrases"></slot>
+    </div>
+    <div class="sidebar">
+      <slot name="sidebar"></slot>
+    </div>
+    <div class="input">
+      <slot name="input"></slot>
+    </div>
+    <div class="history">
+      <slot name="history"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.grid-container {
+  flex: 2;
+  padding: 30px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1.5fr 1fr 1fr;
+  gap: 3% 3%;
+  grid-template-areas:
+    "phrases phrases phrases sidebar"
+    "input input input sidebar"
+    "history history history sidebar";
+}
+.phrases {
+  grid-area: phrases;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  border-radius: 30px;
+}
+.sidebar {
+  grid-area: sidebar;
+  box-shadow: 0 0 10px $dark_color;
+  border-radius: 20px;
+}
+.input {
+  grid-area: input;
+}
+.history {
+  grid-area: history;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
-  <main class="home">
-    <play-button sound="day1_1.mp3"></play-button>
-    <user-input phrase="Il fait beau aujourd'hui"></user-input>
-  </main>
+  <layout-main>
+    <template v-slot:phrases>
+      <play-button sound="day1_1.mp3"></play-button>
+    </template>
+    <template v-slot:input>
+      <user-input phrase="Il fait beau aujourd'hui"></user-input>
+    </template>
+
+    <template v-slot:history>
+      <p>Here's some contact info</p>
+    </template>
+    <template v-slot:sidebar>
+      <p>Here's some contact info</p>
+    </template>
+  </layout-main>
 </template>
 
 <script>
 import PlayButton from "@/components/AppMain/PlayButton";
 import UserInput from "@/components/AppMain/UserInput";
+import LayoutMain from "@/layouts/LayoutMain";
 
 export default {
   name: "Home",
   components: {
+    LayoutMain,
     PlayButton,
     UserInput,
   },
